489a9cb2e55b0a954467fbda7e2469b5093d0161
carstendirks <carstendirks@gmail.com>
2020-09-21 10:07:47 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bitte einen Reload machen! Echt. Wirklich! dcdcdc
         </p>
         <a
           className="App-link"
