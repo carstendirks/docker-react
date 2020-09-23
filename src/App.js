13c63f2f00f4e4491ca2bda48cc6b50375a2e7bd
carstendirks <carstendirks@gmail.com>
2020-09-23 12:49:23 +0200

    changed text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bitte einen Reload machen! Echt. Wirklich! dcdcdc
+          Carsten was here
         </p>
         <a
           className="App-link"
